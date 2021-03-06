a6b7d663e196954a99c20759ff450051f0d00680
jiggy2204 <jignacio2204@gmail.com>
2021-03-06 16:29:11 -0500

    fetchful use this updated one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import fetch from './fetch'
-import Prism from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-json';
-import 'prismjs/themes/prism-tomorrow.css';
-import './App.css'
+import React, { Component } from "react";
+import fetch from "./fetch";
+import Prism from "prismjs/components/prism-core";
+import "prismjs/components/prism-json";
+import "prismjs/themes/prism-tomorrow.css";
+import "./App.css";
 
 const initialState = {
   url: null,
@@ -11,100 +11,100 @@ const initialState = {
   data: null,
   error: null,
   loading: false,
-}
+};
 
 class App extends Component {
   state = initialState;
 
   performFetch = () =>
-    fetch('http://localhost:8000/pokemon?name=pik&type=Electric', {
+    fetch("http://localhost:8000/pokemon?type=Electric&name=pika", {
       headers: {
-        Authorization: 'Bearer MY-API-TOKEN',
+        Authorization: "Bearer fe6df062-b958-4951-903f-cbd098bc9242",
       },
     });
 
   handleClickFetch = (e) => {
     this.performFetch()
-      .then(res => {
+      .then((res) => {
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
-          return res.json().then(err => Promise.reject(err))
+          return res.json().then((err) => Promise.reject(err));
         }
       })
-      .then(data => this.setState({ data, loading: false }))
-      .catch(error => this.setState({ error, loading: false }))
-  }
+      .then((data) => this.setState({ data, loading: false }))
+      .catch((error) => this.setState({ error, loading: false }));
+  };
 
   componentDidMount() {
-    fetch.subscribe(
-      ({ url, options }) => this.setState({ url, options, loading: true })
-    )
+    fetch.subscribe(({ url, options }) =>
+      this.setState({ url, options, loading: true })
+    );
   }
 
   componentDidUpdate() {
-    setImmediate(() => Prism.highlightAll())
+    setImmediate(() => Prism.highlightAll());
   }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Fetchful</h1>
-        <section className='Buttons'>
-          <button
-            className='success'
-            onClick={this.handleClickFetch}>
+        <section className="Buttons">
+          <button className="success" onClick={this.handleClickFetch}>
             fetch
-          </button>
-          {' '}
+          </button>{" "}
           <button
-            className='warning'
-            onClick={() => this.setState(initialState)}>
+            className="warning"
+            onClick={() => this.setState(initialState)}
+          >
             clear
           </button>
         </section>
-        <section className='card Request'>
+        <section className="card Request">
           <header>
             <h3>Request Options</h3>
             <p>
-              <strong className='url-label'>URL:</strong>
-              {' '}
-              <span className='url'>
-                {this.state.url}
-              </span>
+              <strong className="url-label">URL:</strong>{" "}
+              <span className="url">{this.state.url}</span>
             </p>
           </header>
-          <pre><code className='language-json'>
+          <pre>
+            <code className="language-json">
               {JSON.stringify(this.state.options, null, 2)}
-          </code></pre>
+            </code>
+          </pre>
           {this.state.loading ? <h4>Request loading....</h4> : null}
         </section>
-        <section className='card Error'>
+        <section className="card Error">
           <header>
             <h3>Error</h3>
           </header>
-          <pre><code className='language-json'>
-            {JSON.stringify(this.state.error, null, 2)}
-          </code></pre>
+          <pre>
+            <code className="language-json">
+              {JSON.stringify(this.state.error, null, 2)}
+            </code>
+          </pre>
         </section>
-        <section className='card Response'>
+        <section className="card Response">
           <header>
             <h3>Response</h3>
             <p>
-              <strong className='url-label'>Length:</strong>
-              {' '}
-              <span className='url'>
+              <strong className="url-label">Length:</strong>{" "}
+              <span className="url">
                 {this.state.data ? this.state.data.length : null}
               </span>
             </p>
           </header>
-          <pre><code className='language-json'>
-            {JSON.stringify(this.state.data, null, 2)}
-          </code></pre>
+          <pre>
+            <code className="language-json">
+              {JSON.stringify(this.state.data, null, 2)}
+            </code>
+          </pre>
         </section>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
